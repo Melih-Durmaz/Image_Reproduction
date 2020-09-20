5e150b38ddc98e452371036804cbd348a622f9ae
Melih-Durmaz <melihdu2000@gmail.com>
2020-09-20 14:39:29 +0300

    initial structures

--- Main_Canvas.py
+++ Main_Canvas.py
@@ -1,20 +1,25 @@
 import numpy
 from tkinter import *
-
+import cv2
+from PIL import ImageTk,Image
 
 class Main_Canvas(Frame):
     __canvas = None
+    __img = None
 
-    def __init__(self):
+    def __init__(self, image_filename):
         super().__init__()
 
         self.__canvas = Canvas(self)
-        canvas = self.initUI()
 
-    def initUI(self):
+        self.initUI(image_filename)
+
+    def initUI(self, image_filename):
 
         self.master.title("Colours")
         self.pack(fill=BOTH, expand=1)
+
+
 
         # canvas = Canvas(self)
 
@@ -31,16 +36,23 @@ class Main_Canvas(Frame):
                                        outline="#fb0", fill="#fb0")
         self.__canvas.create_rectangle()
 
-    def draw_image(self,filename):
-        img = PhotoImage(file="ball.ppm")
-        canvas.create_image(20, 20, anchor=NW, image=img)
+    def draw_image(self, image_filename):
+        load = Image.open(image_filename)
+        load = load.resize((500, 500))
+        # load = cv2.imread(image_filename)
+        render = ImageTk.PhotoImage(load)
+        img = Label(self, image=render)
+        img.image = render
+        img.place(x=0, y=0)
 
 
 def main():
-
+    image = "images/iron.jpg"
     root = Tk()
-    ex = Main_Canvas()
-    root.geometry("400x100+300+300")
+    mc = Main_Canvas(image)
+    root.geometry("1000x500+300+300")
+    mc.draw_image(image)
+
     root.mainloop()
 
 
